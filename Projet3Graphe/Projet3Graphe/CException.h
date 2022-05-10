162132fa2e6abbe7393471feb130d3f0b9fa2339
Bloow <thomas.raymond-2@etu.univ-tours.fr>
2022-05-10 16:47:13 +0200

    Ajout CException

---
+++ Projet3Graphe/Projet3Graphe/CException.h
@@ -0,0 +1,121 @@
+#ifndef CEXCEPTION_H
+#define CEXCEPTION_H
+
+/* ===== ERROR CODES ===== */
+#define EXCEPTION_INCONNUE 0
+
+/*
+ * Exception class
+ * 
+ * This class is used to handle exceptions.
+ */
+class CException
+{
+    private :
+        unsigned int uiEXCCode;
+        char* pcEXCMessage;
+  
+    public:
+
+        /* *********************************************************
+         *                 Constructeur par defaut                 *
+         ***********************************************************
+         * Entrée: -                                               *
+         * Nécessite: -                                            *
+         * Sortie: -                                               *
+         * Entraine: L'objet en cours est initialisé avec l'erreur *
+         *           EXCEPTION_INCONNUE (0) et le message :         *
+         *           "Erreur initialisée par defaut".              *
+         ********************************************************* */
+        CException()
+        {
+            uiEXCCode = EXCEPTION_INCONNUE;
+            pcEXCMessage = (char*) "Erreur non definie";
+        }
+
+        /* *********************************************************
+         *               Constructeur par parametre                *
+         ***********************************************************
+         * Entrée: unsigned int uiCode                             *
+         * Nécessite: -                                            *
+         * Sortie: -                                               *
+         * Entraine: L'objet en cours est initialisé avec l'erreur *
+         *           uiCode et le message :                        *
+         *           "*Message d'erreur manquant*".                    *
+         ********************************************************* */
+        CException(unsigned int uiCode)
+        {
+            uiEXCCode = uiCode;
+            pcEXCMessage = (char*) "*Message d'erreur manquant*";
+        }
+
+        /* *********************************************************
+         *               Constructeur par parametre                *
+         ***********************************************************
+         * Entrée: unsigned int uiCode                             *
+         *         char* pcMessage                                  * 
+         * Nécessite: -                                            *
+         * Sortie: -                                               *
+         * Entraine: L'objet en cours est initialisé avec l'erreur *
+         *           uiCode et le message sMessage.                *
+         ********************************************************* */
+        CException(unsigned int uiCode, char* pcMessage)
+        {
+            uiEXCCode = uiCode;
+            pcEXCMessage = pcMessage;
+        }
+
+        /* *********************************************************
+         *              Accesseur uiEXCCode (lecture)              *
+         ***********************************************************
+         * Entrée: -                                               *
+         * Nécessite: -                                            *
+         * Sortie: unsigned int uiEXCCode                          *
+         * Entraine: Retourne l'erreur de l'objet en cours.        *
+         ********************************************************* */
+        unsigned int EXCLireCode()
+        {
+            return uiEXCCode;
+        }
+
+        /* *********************************************************
+         *              Accesseur uiEXCCode (écriture)             *
+         ***********************************************************
+         * Entrée: unsigned int uiCode                             *
+         * Nécessite: -                                            *
+         * Sortie: -                                               *
+         * Entraine: Modifie l'erreur de l'objet en cours          *
+         ********************************************************* */
+        void EXCModifierCode(unsigned int uiCode)
+        {
+            uiEXCCode = uiCode;
+        }
+
+        /* *********************************************************
+         *            Accesseur pcEXCMessage (lecture)             *
+         ***********************************************************
+         * Entrée: -                                               *
+         * Nécessite: -                                            *
+         * Sortie: char* pcEXCMessage                              *
+         * Entraine: Retourne le message de l'objet en cours       *
+         ********************************************************* */
+        char* EXCLireMessage()
+        {
+            return pcEXCMessage;
+        }
+
+        /* *********************************************************
+         *            Accesseur pcEXCMessage (écriture)             *
+         ***********************************************************
+         * Entrée: char* pcMessage                                  *
+         * Nécessite: -                                            *
+         * Sortie: -                                               *
+         * Entraine: Modifie le message de l'objet en cours        *
+         ********************************************************* */
+        void EXCModifierMessage(char* pcMessage)
+        {
+            pcEXCMessage = pcMessage;
+        }
+};
+
+#endif
