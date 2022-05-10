0f67dcc9e4021f0abde43235c4780e6e2efb3bb3
Bloow <thomas.raymond-2@etu.univ-tours.fr>
2022-05-10 17:11:51 +0200

    Ajout CArc CGraph CSommet

---
+++ Projet3Graphe/Projet3Graphe/CSommet.h
@@ -0,0 +1,23 @@
+#ifndef CSOMMET_H
+#define CSOMMET_H
+
+#include "CArc.h"
+
+/* ===== ERROR CODES ===== */
+
+/* 
+ * Classe CSommet
+ *
+ * Cette class est utilisée pour gérer les sommets.
+ */
+class CSommet
+{
+    private :
+        unsigned int uiSOMNumero;
+        CArc** ppARCSOMEntrant;
+        CArc** ppARCSOMSortant;
+
+    /* =-=-=-=-=-= La suite dans le prochain épisode =-=-=-=-=-= */
+};
+
+#endif // CSOMMET_H
