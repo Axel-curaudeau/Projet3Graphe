ec77e593ddbf85dee28653caa6bc2543b46de0c7
Bloow <thomas.raymond-2@etu.univ-tours.fr>
2022-05-11 18:07:08 +0200

    Fin CArc + debut CSommet.h

--- Projet3Graphe/Projet3Graphe/CArc.cpp
+++ Projet3Graphe/Projet3Graphe/CArc.cpp
@@ -1,6 +1,27 @@
 #include "CArc.h"
 
-CArc::CArc(unsigned int uiDestination)
-{
+/* *********************************************************
+ *               Constructeur par paramètre                *
+ ***********************************************************
+ * Entrée: unsigned int uiDestination                      *
+ * Nécessite: -                                            *
+ * Sortie: -                                               *
+ * Entraine: L'objet en cours est initialisé la valeur de  *
+ *           uiDestination.                                *
+ ********************************************************* */
+CArc::CArc(unsigned int uiDestination) {
     uiARCDestination = uiDestination;
+}
+
+/* *********************************************************
+ *                 Getter uiARCDestination                 *
+ ***********************************************************
+ * Entrée: -                                               *
+ * Nécessite: -                                            *
+ * Sortie: unsigned int uiARCDestination                   *
+ * Entraine: La valeur de uiARCDestination est initialisée *
+ *           avec celle de uiDestination.                  *
+ ********************************************************* */
+unsigned int CArc::ARCGetDest() {
+    return uiARCDestination;
 }
