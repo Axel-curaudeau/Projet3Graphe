9e416d436439ac7bee88b8adc02ce13507e581c8
Axel Curaudeau <axelcuraudeau@gmail.com>
2022-05-28 11:41:54 +0200

    Fonction main

--- Projet3Graphe/Projet3Graphe/Projet3Graphe.cpp
+++ Projet3Graphe/Projet3Graphe/Projet3Graphe.cpp
@@ -11,52 +11,25 @@ using namespace std;
 
 int main(int argc, char* argv[])
 {
-    try{
-        CGraphe* graphe = new CGraphe();
-        graphe->GRPAjouterSommet(1);
-        graphe->GRPAjouterSommet(2);
-        graphe->GRPAjouterSommet(3);
-        graphe->GRPAjouterSommet(4);
-        graphe->GRPAjouterSommet(5);
-        graphe->GRPAjouterSommet(6);
-        graphe->GRPAjouterSommet(7);
-        graphe->GRPAjouterSommet(8);
-        graphe->GRPModifierSommet(7, 12);
-        graphe->GRPAjouterArc(1, 2);
-        graphe->GRPAjouterArc(1, 3);
-        graphe->GRPAjouterArc(1, 4);
-        graphe->GRPAjouterArc(2, 5);
-        graphe->GRPAjouterArc(2, 6);
-        graphe->GRPAjouterArc(3, 12);
-        graphe->GRPAjouterArc(3, 8);
-        graphe->GRPAjouterArc(4, 8);
-        graphe->GRPAjouterArc(5, 8);
-        graphe->GRPAjouterArc(6, 8);
-        graphe->GRPAjouterSommet(9);
-        graphe->GRPAjouterSommet(10);
-        graphe->GRPAjouterSommet(11);
-        graphe->GRPAjouterArc(10, 11);
+    if (argc == 2) {
+        try {
+            CGraphe graphe;
+            CLexeur fichier(argv[1]);
+            CAnalyseur analyse(&fichier);
+            analyse.ANLLireSommets(graphe);
+            analyse.ANLLireArcs(graphe);
+            graphe.GRPAfficher();
 
-        graphe->GRPAfficher();
-        graphe->GRPGenererGraphviz();
+            graphe.GRPGenererGraphviz();
 
-        COperationGraphe OPE1;
-        CGraphe grapheinverse = OPE1.OPEInverserGraphe(*graphe);
-        grapheinverse.GRPGenererGraphviz();
-        
-
-        delete graphe;
-
-        CGraphe graphe2;
-        CLexeur fichier((char*)"graphes/graphetest.txt");
-        CAnalyseur analyse(&fichier);
-        cout << analyse.ANLLireNbArcs() << "|" << analyse.ANLLireNbSommets() << endl;
-        analyse.ANLLireSommets(graphe2);
-        analyse.ANLLireArcs(graphe2);
-        graphe2.GRPGenererGraphviz();
+            COperationGraphe OPG;
+            CGraphe graphe2 = OPG.OPEInverserGraphe(graphe);
+            graphe2.GRPAfficher();
+        }
+        catch (CException EXCE) {
+            cout << EXCE.EXCLireMessage() << endl;
+        }
     }
-    catch (CException EXCE) {
-        cout << EXCE.EXCLireMessage() << endl;
-    }
+    
     return 0;
 }
